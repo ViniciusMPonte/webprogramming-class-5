bc49acf3b1b8e288c39041543ee74339b3f7941e
Vinícius Pontes <vinicius.mponte@gmail.com>
2024-09-09 17:53:23 -0300

    Update code

--- js/index.js
+++ js/index.js
@@ -1,2 +1,11 @@
 document.getElementById("firstP").innerHTML = "Primeiro Parágrafo";
 
+function sum (){
+    let num1 = parseFloat(document.getElementById("idNum1").value)
+    let num2 = parseFloat(document.getElementById("idNum2").value)
+
+    let result = num1 + num2
+
+    document.getElementById("idRes").value = result
+
+}
