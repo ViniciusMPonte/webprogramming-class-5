4bcce907047f2cc8e012ca0fbf62fb0c719f2394
Vinícius Pontes <vinicius.mponte@gmail.com>
2024-09-09 18:12:45 -0300

    Updated code.

--- js/index.js
+++ js/index.js
@@ -1,10 +1,34 @@
-document.getElementById("firstP").innerHTML = "Primeiro Parágrafo";
+document.getElementById("firstP").innerHTML = "Calculadora";
 
-function sum (){
+function calculate (){
     let num1 = parseFloat(document.getElementById("idNum1").value)
     let num2 = parseFloat(document.getElementById("idNum2").value)
+    
+    let operator = document.getElementById("operator").value
+    let result
 
-    let result = num1 + num2
+    switch (operator) {
+        case '+':
+            result = num1 + num2
+            break;
+        case '-':
+            result = num1 - num2
+            break;
+        case '*':
+            result = num1 * num2
+            break;
+        case '/':
+            if (num2 !== 0) {
+                result = num1 / num2
+            } else {
+                result = 'Erro: divisão por zero'
+            }
+            break;
+        default:
+            result = 'Operação inválida'
+    }
+
+    
 
     document.getElementById("idRes").value = result
 
